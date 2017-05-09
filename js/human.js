fcd5de7829975140809cc21d3b054cc2c19fb73f
rrouis <R@MBPR0104.local>
2017-05-09 22:46:36 +0900

    last
    last

--- js/human.js
+++ js/human.js
@@ -7,4 +7,28 @@ $(function(){
 
   // フリッキーマスター
   $('.main-gallery').flickity();
+
+});
+
+
+$(function(){
+
+  $(window).scroll(function(){
+    // スクロールの座標
+    var scrollPos = $(this).scrollTop();
+    console.log("scrollPos:" + scrollPos);
+
+    if(scrollPos >= 0){
+      $('#fadeLogo').addClass('fadeIn');
+      console.log("addClass");
+    }
+    if(scrollPos >= 500){
+      $('#fadeLead').addClass('fadeIn');
+      console.log("addClass");
+    }
+    if(scrollPos >= 900){
+      $('#fadeMain').addClass('fadeIn');
+      console.log("addClass");
+    }
+  });
 });
